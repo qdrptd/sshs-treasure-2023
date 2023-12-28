4b488851855e01b59afd934d5b1692bf694a8d90
qdrptd <99246971+qdrptd@users.noreply.github.com>
2023-12-29 00:56:57 +0900

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 import { getFunctions } from "firebase/functions"
+import { firebaseConfig } from "./firebaseconfig.js"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAzdhn7pea1M7fsqUdjFvU9jpox1PdcWcs",
-  authDomain: "sshs-treasure-2023.firebaseapp.com",
-  projectId: "sshs-treasure-2023",
-  storageBucket: "sshs-treasure-2023.appspot.com",
-  messagingSenderId: "218461744402",
-  appId: "1:218461744402:web:9750550221528a5f0b57cb"
-};
 
 
 export const app = initializeApp(firebaseConfig)
