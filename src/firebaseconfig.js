8e8dc3f21417584c32edbb0ac3b43b5bdbb91c11
qdrptd <99246971+qdrptd@users.noreply.github.com>
2024-01-11 18:34:35 +0900

    removed private datum

--- src/firebaseconfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAzdhn7pea1M7fsqUdjFvU9jpox1PdcWcs",
-    authDomain: "sshs-treasure-2023.firebaseapp.com",
-    projectId: "sshs-treasure-2023",
-    storageBucket: "sshs-treasure-2023.appspot.com",
-    messagingSenderId: "218461744402",
-    appId: "1:218461744402:web:9750550221528a5f0b57cb"
-  };
